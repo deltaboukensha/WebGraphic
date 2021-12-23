4d004b86a920395606f66361010340fe44df458d
DeltaBoukensha <DeltaBoukensha@gmail.com>
2021-12-23 21:50:07 +0100

    fix coords

--- source/rippleEffect/rendering.frag.glsl
+++ source/rippleEffect/rendering.frag.glsl
@@ -6,7 +6,7 @@ uniform sampler2D backgroundSampler;
 out vec4 fragment;
 
 void main() {
-  vec4 backgroundColor = texture(backgroundSampler, st);
-  float height = texture(heightSampler, vec2(st.s, st.t)).r;
-  fragment = vec4(backgroundColor, 0, 0, 1);
+  vec4 backgroundSample = texture(backgroundSampler, st);
+  vec4 heightSample = texture(heightSampler, st);
+  fragment = vec4(backgroundSample, 0, 0, 1);
 }
